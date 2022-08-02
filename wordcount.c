3e5de0141c033099727b7f9f48b89ee78c93d14b
Moustaphasow01 <sowmoustaphayaya@gmail.com>
2022-08-02 09:22:02 +0200

    fct that count the number of word from users input

---
+++ wordcount.c
@@ -0,0 +1,31 @@
+#include "simple_shell.h"
+
+/**
+ * word_count - fct that count the number of word from user's input
+ *
+ * @str: input's string
+ *
+ * Return: Number of words
+ */
+size_t word_count(char *str)
+{
+	size_t i = 0, words = 0;
+	int flag = OUT;
+
+	while (str && str[i])
+	{
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+		{
+			if (str[i] == '\n')
+				str[i] = '\0';
+			flag = OUT;
+		}
+		else if (flag == OUT)
+		{
+			flag = IN;
+			words++;
+		}
+		i++;
+	}
+	return (words);
+}
